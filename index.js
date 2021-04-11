eeb13f75a143270693249576f399f9f82b321c2b
Renu Shinde <renunikhilp@gmail.com>
2021-04-11 22:01:13 +0000

    Problem 2

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ function reverseArr (arr) {
 }
 console.log(reverseArr([2, 3, 4]));
 
-// 
+// Problem 2 : Reverse a string
+
+function reverseStr (str) {
+  let newStr = "";
+  newStr = str.split("").reverse().join("");
+  return newStr;
+
+}
+console.log("Reverse a string using array ", reverseStr("Hello")); 
+
+// Without using any methods
+
+function reverseAStr(str) {
+let newStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  newStr += str[i];
+} 
+return newStr;
+}
+console.log("Reverse a string Without using any methods ",  reverseAStr("Hello world")); 
