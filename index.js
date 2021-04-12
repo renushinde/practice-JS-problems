c67f0b8d25701175ece7914987837f9e6e67b36f
Renu Shinde <renunikhilp@gmail.com>
2021-04-12 23:04:58 +0000

    max number in array

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ function reverseArr (arr) {
 }
 console.log(reverseArr([2, 3, 4]));
 
+// Largest integer in the array
+function maxNum (arr) {
+  let maxNumber = arr[0];
+  for ( let i = 0; i < arr.length; i ++) {
+    if ( arr[i] > maxNumber) {  
+        maxNumber = arr[i];
+    }
+      
+  }
+return maxNumber;
+}
+console.log("Max number is ", maxNum([2,3,4,5]));
+
 // Problem 2 : Reverse a string
 
 function reverseStr (str) {
