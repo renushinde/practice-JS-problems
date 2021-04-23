76e8dd09d6216ac5ee6c905bb2fd4508cb9c8fd1
Renu Shinde <renunikhilp@gmail.com>
2021-04-23 00:33:45 +0000

    find the duplicate

--- index.js
+++ index.js
@@ -198,4 +198,23 @@ function removeVowels(str) {
 return newArr.join("");
 }
 console.log("New String with Vowels removed:", removeVowels('Hello')); // hll
-//////
+
+// Find the duplicate
+function findTheDuplicate(arr){
+  let counter = {}
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] in counter){
+      counter[arr[i]]++;
+     
+    } else {
+      counter[arr[i]] = 1;
+      console.log(counter);
+    }
+  }
+  for(let key in counter){
+    if(counter[key] > 1) return +key
+   
+  }
+    
+}
+console.log(findTheDuplicate([1,2,3,3,1,4,3,12]));
